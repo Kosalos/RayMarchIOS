0da79255a591d9072f853c684bf6463858a82341
Harry Kosalos <harrykosalos@Harrys-iMac.local>
2018-02-01 12:21:55 -0800

    added so device sizes can be visualized during programmatic layout development

--- Source/Background.swift
+++ Source/Background.swift
@@ -6,7 +6,7 @@ class Background: UIView {
 
 //    override func draw(_ rect: CGRect) {
 //        super.draw(rect)
-//        
+//
 //        let color = UIColor(red:0.2, green:0.2, blue:0.2, alpha: 1)
 //        color.setFill()
 //        UIBezierPath(rect:CGRect(x:0, y:0, width:xs, height:ys)).fill()
